ed4113180e301cc504596fb487822b5d1f1e41fa
ryunosinfx <ryunosinfx@livedoor.com>
2019-01-23 09:48:21 +0900

    dekitatokoromade

--- test/test.js
+++ test/test.js
@@ -1,17 +1,21 @@
 import { idbw } from '../src/indexeddbWrapper'
-
+import { TestUtil } from './testUtil'
 export class indexeddbWrapperTest {
 	constructor() {
 		this.db = new idbw("indexeddbWrapper-test");
+		this.logger= new TestUtil('idbw test!');
 	}
 	execAll() {
 		alert("test!");
 	}
-	//test
+	//test-----------------------------------------------
 	//createDB
 	//creatTable
 	//bulkInsertUpdate
 	//select
 	//_updateExecute
 	//delete
+	test1(){
+		this.db.getObAccessor('test','pk');
+	}
 }
--- test/testUtil.js
+++ test/testUtil.js
@@ -1,4 +1,4 @@
-export class testUtil {
+export class TestUtil {
 	constructor(msg) {
 		this.frame = document.createElement('div');
 		const titleRow = document.createElement('div');
@@ -10,6 +10,7 @@ export class testUtil {
 		this.frame.appendChild(hr);
 		this.currentFrame = document.createElement('div');
 		this.frame.appendChild(this.currentFrame);
+		this.time= Date.now();
 	}
 	createNewFrame() {
 		const hr = document.createElement('hr');
@@ -24,7 +25,24 @@ export class testUtil {
 		this.currentFrame.appendChild(title);
 		this.currentFrame.appendChild(hr);
 	}
-	logInfo(msg) {
-
+	log(level,time,msg) {
+		const row = document.createElement('div');
+		row.textContent = time+' '+level+' '+msg;
+		this.currentFrame.appendChild(row);
+	}
+	getDuration(){
+		const time = Date.now();
+		const duration = time - this.time;
+		this.time + time;
+		return time+'/'+("00000000000"+duration).substring(-10);
+	}
+	info(msg) {
+		this.log("[INFO]",this.getDuration(),msg);
+	}
+	warn(msg) {
+		this.log("[WARN]",this.getDuration(),msg);
+	}
+	error(msg) {
+		this.log("[ERROR]",this.getDuration(),msg);
 	}
 }
